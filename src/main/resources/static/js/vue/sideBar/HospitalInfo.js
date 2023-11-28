0f194828ae096397a2d9008553869b3bbc469543
vsk <v.skoptsova@dotsource.de>
2023-11-28 15:22:18 +0100

    info page

--- src/main/resources/static/js/vue/sideBar/HospitalInfo.js
+++ src/main/resources/static/js/vue/sideBar/HospitalInfo.js
@@ -11,34 +11,80 @@ export default {
   <div class="turn_items appointment_header">
     <div class="emergency_wrapper">
       <div class="emergency">
-        <div class="radius_wrapper emergency_item_wrapper">
+        <div class="radius_wrapper info_item_wrapper">
           <div class="carousel_item">
-            <img src="/images/emergency.png" alt="Patients" style="width: 20vw;">
+            <img src="/images/hospital.png" alt="Patients" style="width: 20vw;">
           </div><br/>
           <div class="carousel_item text-center">
-            <h3 class="text-white">Emergency?</h3>
-            <h3 class="text-white">We can help you!</h3>
+            <h3 class="text-white">Virtual Healthcare</h3>
+            <h3 class="text-white">for you!</h3>
           </div>
         </div>
         <br/>
 
-        <div class="radius_wrapper emergency_item_wrapper2">
-          <div class="alert">
-            <img src="/images/alert_9418835.png" alt="Patients" style="width: 11vw;">
-          </div><br/>
-          <div class="carousel_item text-center">
-            <h3 class="text-white"><b>Emergency?</b><br/><br/>
-            <b>Medical Service: 112</b><br/><br/>
-            <div class="emergency_text_wrapper">KV-Notarztdienst (Emergency doctor service provided by health insurance): 116 117
-            Poison Emergency Call: +49 361 – 730730
-            University of Leipzig Medical Center (24/7):
-            +49 341 – 97 109
-            Emergency Department University of Leipzig Medical Center
-            +49 341 –  97 17800
-            Paul-List-Straße 27
-            04103 Leipzig
+        <div class="radius_wrapper info_item_wrapper2 info_item">
+        <div class="turn_items">
+        <div class="carousel_wrapper">
+          <div class="carousel_info">
+            <div class="bg-white radius_wrapper carousel_item_wrapper_info">
+              <div class="carousel_item">
+                <img src="/images/patient_3359120 (2).png" alt="Patients">
+              </div><br/>
+              <div class="carousel_item text-center">
+                <h6 class="text-secondary">Search a Doctor</h6>
+              </div>
             </div>
-          </div><br/>
+            <br/>
+
+            <div class="bg-white radius_wrapper carousel_item_wrapper_info">
+              <div class="carousel_item">
+                <img src="/images/bed_3359393 (1).png" alt="Cabinets">
+              </div><br/>
+              <div class="carousel_item text-center">
+                <h6 class="text-secondary">Online Appointment</h6>
+              </div>
+            </div>
+            <br/>
+
+            <div class="bg-white radius_wrapper carousel_item_wrapper_info">
+              <div class="carousel_item">
+                <img src="/images/medical-mask_3359145 (2).png" alt="Staff">
+              </div><br/>
+              <div class="carousel_item text-center">
+                <h6 class="text-secondary">Emergency information</h6>
+              </div>
+            </div>
+            <br/>
+
+            <div class="bg-white radius_wrapper carousel_item_wrapper_info">
+              <div class="carousel_item">
+                <img src="/images/stethoscope_3359379 (1).png" alt="Appoinments">
+              </div><br/>
+              <div class="carousel_item text-center">
+                <h6 class="text-secondary">Details information</h6>
+              </div>
+            </div>
+            <br/>
+
+            <div class="bg-white radius_wrapper carousel_item_wrapper_info">
+              <div class="carousel_item">
+                <img src="/images/measuring_3359420 (1).png" alt="Pharmasy">
+              </div><br/>
+              <div class="carousel_item text-center">
+                <h6 class="text-secondary">Online Pharmasy</h6>
+              </div>
+            </div>
+
+            <div class="bg-white radius_wrapper carousel_item_wrapper_info">
+              <div class="carousel_item">
+                <img src="/images/patient_3359120 (2).png" alt="Pharmasy">
+              </div><br/>
+              <div class="carousel_item text-center">
+                <h6 class="text-secondary">Health Procedures</h6>
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
